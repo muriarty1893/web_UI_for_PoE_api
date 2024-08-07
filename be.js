f7544ff88dbd27f04bf0dcaec0c33f7463633469
Murat Eker <murateker9166@gmail.com>
2024-08-07 22:14:03 +0300

    added div/chaos ratio

--- be.js
+++ be.js
@@ -1,12 +1,10 @@
-// backend
 const express = require("express");
 const path = require("path");
-const { NinjaAPI, WatchAPI } = require("poe-api-manager");
+const { NinjaAPI } = require("poe-api-manager");
 const app = express();
 const port = 3000;
 
 const ninjaAPI = new NinjaAPI("Settlers");
-const requestedProperties = ["id", "name", "divineValue", "explicitModifiers", "icon"];
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
@@ -16,14 +14,13 @@ app.get("/fetch-item-price", async (req, res) => {
     const iname = req.query.iname.toLowerCase().trim(); 
 
     try {
-        const theItem = await ninjaAPI.itemView.uniqueAccessory.getData(requestedProperties);
+        const theItem = await ninjaAPI.itemView.uniqueAccessory.getData(["id", "name", "divineValue", "explicitModifiers", "icon"]);
 
         const itemOut = theItem.find(item => item.name.toLowerCase() === iname);
 
         if (itemOut) {
             res.json({ success: true, data: itemOut });
         } else {
-
             const similarItems = theItem.filter(item => item.name.toLowerCase().includes(iname));
             if (similarItems.length > 0) {
                 res.json({ success: true, message: `Item not found. Did you mean one of these?`, data: similarItems });
@@ -31,6 +28,15 @@ app.get("/fetch-item-price", async (req, res) => {
                 res.json({ success: false, message: `${req.query.iname} not found.` });
             }
         }
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+});
+
+app.get("/fetch-chaos-value", async (req, res) => {
+    try {
+        const chaosValue = await ninjaAPI.currencyView.currency.getQuickCurrency("Divine Orb");
+        res.json({ success: true, chaosValue });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
